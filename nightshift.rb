068261cdb8cafe62f75164070e0cd416ba6d40a1
Oliver Schrenk <oliver.schrenk@gmail.com>
2023-12-08 17:10:07 -0600

    nightshift: Fix homepage

--- nightshift.rb
+++ nightshift.rb
@@ -1,6 +1,6 @@
 class Nightshift < Formula
   desc "Control Night Shift settings from CLI"
-  homepage "https://github.com/oschrenk/mightshift.swift"
+  homepage "https://github.com/oschrenk/nightshift.swift"
   url "https://github.com/oschrenk/nightshift.swift/archive/refs/tags/v0.1.tar.gz"
   license "MIT"
   sha256 "d21d59b5bcc00fb9039ae2eed27470e5935323789a6c926ac1846eba86581957"
