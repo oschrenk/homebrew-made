9d9d78c524edf7a13f87bde2655515813ba0cc9d
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-01-23 19:04:11 -0600

    :rocket: sessionizer v0.2

--- sessionizer.rb
+++ sessionizer.rb
@@ -2,20 +2,20 @@ class Sessionizer < Formula
   desc "Tmux session manager"
   homepage "https://github.com/oschrenk/sessionizer"
   license "APL"
-  head "https://github.com/oschrenk/sessionizer/releases/tag/v0.1", branch: "main"
+  head "https://github.com/oschrenk/sessionizer/releases/tag/v0.2", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oschrenk/sessionizer/releases/download/v0.1/sessionizer-darwin-arm64"
-      sha256 "80846254b4086a56716d5a06d44cdf7ddc993b56759a64c8c9ffd74e17b7a6ba"
+      url "https://github.com/oschrenk/sessionizer/releases/download/v0.2/sessionizer-darwin-arm64"
+      sha256 "94537b5fac7e8830f6869242824b9646897acc9c56bde069638f2c1624c51b43"
 
       def install
         bin.install "sessionizer-darwin-arm64" => "sessionizer"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oschrenk/sessionizer/releases/download/v0.1/sessionizer-darwin-amd64"
-      sha256 "ec9b2482d49f71d61cf429ceb22b630bd896d14b6572486fa881d72c93be1802"
+      url "https://github.com/oschrenk/sessionizer/releases/download/v0.2/sessionizer-darwin-amd64"
+      sha256 "9bd05d2e4cf3027f528374debb23fd679ebf6f858fe4efcba1a7c23144cc7496"
 
       def install
         bin.install "sessionizer-darwin-amd64" => "sessionizer"
