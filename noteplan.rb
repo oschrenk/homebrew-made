d7b50fd8d0abbaa2bea223b2b63cd857ae7359f6
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-02-04 23:46:40 +0100

    noteplan: 0.2

--- noteplan.rb
+++ noteplan.rb
@@ -3,19 +3,20 @@ class Noteplan < Formula
   homepage "https://github.com/oschrenk/noteplan"
   license "APL"
   head "https://github.com/oschrenk/noteplan", branch: "main"
+  version "0.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oschrenk/noteplan/releases/download/v0.1/noteplan-darwin-arm64"
-      sha256 "df20667bcc660a0308146e9050a8d180748ac866306fd0feeaa30f10986816f7"
+      url "https://github.com/oschrenk/noteplan/releases/download/v0.2/noteplan-darwin-arm64"
+      sha256 "c7140e2786008e62817fa142e71d4d47ab61fd9eb84da81e9f17f0c548175261"
 
       def install
         bin.install "noteplan-darwin-arm64" => "noteplan"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oschrenk/noteplan/releases/download/v0.1/noteplan-darwin-amd64"
-      sha256 "c72f21f8c2d481e1fdb1e25bbc28a2039c637c40a987258f0c8cca132d1d5b7c "
+      url "https://github.com/oschrenk/noteplan/releases/download/v0.2/noteplan-darwin-amd64"
+      sha256 "06e876553767c75a36a2137d4b0f7d3c6d9ced9c76b51828451de171a5a0fc0e"
 
       def install
         bin.install "noteplan-darwin-amd64" => "noteplan"
