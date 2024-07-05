46cda7094d2d8c0d85618a18d0144d285192e163
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-05 16:52:40 -0600

    :rocket: Release mission 0.2.1

--- mission.rb
+++ mission.rb
@@ -3,20 +3,20 @@ class Mission < Formula
   homepage "https://github.com/oschrenk/mission"
   license "APL"
   head "https://github.com/oschrenk/mission", branch: "main"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/oschrenk/mission/releases/download/v0.2.0/mission-darwin-amd64"
-      sha256 "f13c6b89879b343f263f7b3f1228df709ff584b85076d0ad3e811083a37c7945"
+      url "https://github.com/oschrenk/mission/releases/download/v0.2.1/mission-darwin-amd64"
+      sha256 "1b3689f0400ef6ce2ed9aca50d93db3fa804fdd81eec0a587f9269355b1e58d3"
 
       def install
         bin.install "mission-darwin-amd64" => "mission"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/oschrenk/mission/releases/download/v0.2.0/mission-darwin-arm64"
-      sha256 "0e91e637e75e42d59517b53e2736fae8b82bd4f4f7f5f7dd21078faf3cb14b90"
+      url "https://github.com/oschrenk/mission/releases/download/v0.2.1/mission-darwin-arm64"
+      sha256 "145c4cbb3ba3b582ed4fade9b154f15f0f56113b003dfbf552ce8f87a5bcace6"
 
       def install
         bin.install "mission-darwin-arm64" => "mission"
