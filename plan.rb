c917ce5c973ac48e709dd1f515c4e13374048512
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-09 16:28:48 +0200

    :bug: Fix version string

--- plan.rb
+++ plan.rb
@@ -8,7 +8,7 @@ class Plan < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/oschrenk/plan.swift/releases/download/v0.2.0/plan-darwin-arm64"
-      sha256 "e3d3c1c15b55eb714a7050c26d29f545d068ed020d8a7bd6d13116942ed64a9c  .build/release/plan-darwin-arm64"
+      sha256 "e3d3c1c15b55eb714a7050c26d29f545d068ed020d8a7bd6d13116942ed64a9c"
 
       def install
         bin.install "plan-darwin-arm64" => "plan"
