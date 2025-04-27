2f1a81fa501b9be1567baab3497cb5e943782788
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-27 21:43:22 +0200

    :rocket: Release 0.12.4

--- plan.rb
+++ plan.rb
@@ -3,15 +3,16 @@ class Plan < Formula
   homepage "https://github.com/oschrenk/plan.swift"
   license "MIT"
   head "https://github.com/oschrenk/plan.swift", branch: "main"
-  version "0.12.3"
+  version "0.12.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oschrenk/plan.swift/releases/download/v#{version}/plan-darwin-arm64"
-      sha256 "ad825e610fee8b0af712081518cab85633fa4516ede3aadc0fe01643868ad5e5"
+      url "https://github.com/oschrenk/plan.swift/releases/download/v#{version}/plan-0.12.4.tar.gz"
+      sha256 "5bbfa708f38c0377eca91bf549d6067b42e19b0b2bf14f81f7d0ae0b20d5760e"
 
       def install
         bin.install "plan-darwin-arm64" => "plan"
+        fish_completion.install "Completions/plan.fish"
       end
     end
   end
