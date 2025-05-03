8086e106f483e63b84c127d219667206bea2641a
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-05-03 12:30:16 +0200

    :rocket: Release mission 0.6.1

--- mission.rb
+++ mission.rb
@@ -3,19 +3,19 @@ class Mission < Formula
   homepage "https://github.com/oschrenk/mission"
   license "APL"
   head "https://github.com/oschrenk/mission", branch: "main"
-  version "0.6.0"
+  version "0.6.1"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/oschrenk/mission/releases/download/v#{version}/mission-darwin-amd64"
-      sha256 "cc7a452595e356249167e3fb32ab60ea1b04e429b879186d12d51fc939ec2e7a"
+      sha256 "06fd29166359bf02d15c73e93662ccb1cee40281568c51490f6e9bc921857711"
       def install
         bin.install "mission-darwin-amd64" => "mission"
       end
     end
     if Hardware::CPU.arm?
       url "https://github.com/oschrenk/mission/releases/download/v#{version}/mission-darwin-arm64"
-      sha256 "58868648ece855bdba07447eb0859f064e9f71ce2e3a45d60773cdd21927c206"
+      sha256 "e5edd0f795d6a7163d522ac2d3fecd180cb7165632fa7044a412508301a66167"
 
       def install
         bin.install "mission-darwin-arm64" => "mission"
