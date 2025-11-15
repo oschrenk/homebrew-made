046f4456704053176ef7dfa4f790b48e6dfc686f
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-11-15 12:23:37 -0600

    :rocket: Release sessionizer 0.7.2

--- sessionizer.rb
+++ sessionizer.rb
@@ -3,19 +3,19 @@ class Sessionizer < Formula
   homepage "https://github.com/oschrenk/sessionizer"
   license "MIT"
   head "https://github.com/oschrenk/sessionizer", branch: "main"
-  version "0.7.1"
+  version "0.7.2"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/oschrenk/sessionizer/releases/download/v#{version}/sessionizer-darwin-amd64"
-      sha256 "66b768dce5397930de9e8e47be0af36fbbe1dc81f90d36d62855af7b72bd45a2"
+      sha256 "15aec5c057ce550e16b32a09d9f157f993003662959ae8b01fd8f943d9ceeb05"
       def install
         bin.install "sessionizer-darwin-amd64" => "sessionizer"
       end
     end
     if Hardware::CPU.arm?
       url "https://github.com/oschrenk/sessionizer/releases/download/v#{version}/sessionizer-darwin-arm64"
-      sha256 "934c6a14f71f921b406d90e5a467b7ca8b0d814fc8746fcc7d9606ba9ddc8597"
+      sha256 "b8312bc48ceb10e5ede6a3d027d4bcf7a52349987fefdbe5effeecf1055e9c86"
       def install
         bin.install "sessionizer-darwin-arm64" => "sessionizer"
       end
