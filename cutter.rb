8beb85031be6b008c865e20dd9a71afcbac62e44
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-01-28 13:06:07 -0600

    :rocket: Release cutter 0.1.1

--- cutter.rb
+++ cutter.rb
@@ -3,19 +3,12 @@ class Cutter < Formula
   homepage "https://github.com/oschrenk/cutter"
   license "MIT"
   head "https://github.com/oschrenk/cutter", branch: "main"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/oschrenk/cutter/releases/download/v0.1.0/cutter-darwin-amd64"
-      sha256 "d19547974f0e14d67420338f86ac9d5debab5bf9cf998631f71343209e0d79e8"
-      def install
-        bin.install "cutter-darwin-amd64" => "cutter"
-      end
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/oschrenk/cutter/releases/download/v0.1.0/cutter-darwin-arm64"
-      sha256 "12d00f73a0ba60048072ded8298e8ed122ec41eb97687a0db03e40287d78f257"
+      url "https://github.com/oschrenk/cutter/releases/download/v#{version}/cutter-darwin-arm64"
+      sha256 "de00f963758573384b8def6ddcbc39b686f2948a1a6c47a07c5891e43a0a70b1"
       def install
         bin.install "cutter-darwin-arm64" => "cutter"
       end
