a4cada86d48a92b232de891e52bf30e48ef9d822
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-01-28 18:05:37 -0600

    :rocket: Release arbol 0.2.0

--- arbol.rb
+++ arbol.rb
@@ -3,12 +3,12 @@ class Arbol < Formula
   homepage "https://github.com/oschrenk/arbol"
   license "MIT"
   head "https://github.com/oschrenk/arbol", branch: "main"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/oschrenk/arbol/releases/download/v#{version}/arbol-darwin-arm64"
-      sha256 "97f1b3ccacaf6fa1e0709e03478e28c86f8bbf679dc2b1bdb319727f183f180a"
+      sha256 "99a26a213746388a8ca06c380756c769e4471a1a269cbd9dce2ff9ed992dd84c"
 
       def install
         chmod "+x", "arbol-darwin-arm64"
