4c25b561742ef03e8fb579377c27cf686b1e8931
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-02-03 11:17:39 -0600

    :rocket: Release nightshift 0.1.1

--- nightshift.rb
+++ nightshift.rb
@@ -1,9 +1,10 @@
 class Nightshift < Formula
   desc "Control Night Shift settings from CLI"
   homepage "https://github.com/oschrenk/nightshift.swift"
-  url "https://github.com/oschrenk/nightshift.swift/archive/refs/tags/v0.1.tar.gz"
+  version "0.1.1"
+  url "https://github.com/oschrenk/nightshift.swift/archive/refs/tags/v#{version}.tar.gz"
   license "MIT"
-  sha256 "d21d59b5bcc00fb9039ae2eed27470e5935323789a6c926ac1846eba86581957"
+  sha256 "ddc8fb6093db212108b36046b64a3d1124251e9412f0f0512547bbf1f3050f96"
   head "https://github.com/oschrenk/nightshift.swift", branch: "main"
 
   def install
@@ -11,5 +12,6 @@ class Nightshift < Formula
         "--configuration", "release",
         "--disable-sandbox"
     bin.install '.build/release/nightshift'
+    generate_completions_from_executable(bin/"nightshift", "--generate-completion-script")
   end
 end
