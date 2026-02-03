63904ad4e4da51ae7047120717197f862c29091a
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-02-03 10:56:37 -0600

    :rocket: Release wallpaper 0.0.3

--- wallpaper.rb
+++ wallpaper.rb
@@ -3,15 +3,16 @@ class Wallpaper < Formula
   homepage "https://github.com/oschrenk/wallpaper.swift"
   license "MIT"
   head "https://github.com/oschrenk/wallpaper.swift", branch: "main"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/oschrenk/wallpaper.swift/releases/download/v#{version}/wallpaper-#{version}.tar.gz"
-      sha256 "af91e3b1656c48272aa71852ad541700ace39da625ac8536427c3fa406adeb3a"
+      sha256 "458afb464a535e854ef2da8ac51554694e140f3416e51af94f589564425bc1cd"
 
       def install
         bin.install "wallpaper-darwin-arm64" => "wallpaper"
+        generate_completions_from_executable(bin/"wallpaper", "--generate-completion-script")
       end
     end
   end
