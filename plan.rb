7de28d49a808b4ac369d83061bd3859fbc36db32
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-02-03 10:39:27 -0600

    :children_crossing: plan - improve shell completions

--- plan.rb
+++ plan.rb
@@ -11,8 +11,9 @@ class Plan < Formula
       sha256 "3578cdfb8e122171db98696230ecca816015d087aed57f296f17275fb191f5a0"
 
       def install
+        chmod "+x", "plan-darwin-arm64"
         bin.install "plan-darwin-arm64" => "plan"
-        fish_completion.install "Completions/plan.fish"
+        generate_completions_from_executable(bin/"plan", "--generate-completion-script")
       end
     end
   end
