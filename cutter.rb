42f341dfa866c5f858d42779fb4efeb5f0909cdd
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-02-03 10:03:07 -0600

    :children_crossing: cutter - install shell completions

--- cutter.rb
+++ cutter.rb
@@ -10,7 +10,9 @@ class Cutter < Formula
       url "https://github.com/oschrenk/cutter/releases/download/v#{version}/cutter-darwin-arm64"
       sha256 "16c9e39621a80c48ba96c9f66996917ea0e1880520114dc81825a7cc25c37244"
       def install
+        chmod "+x", "cutter-darwin-arm64"
         bin.install "cutter-darwin-arm64" => "cutter"
+        generate_completions_from_executable(bin/"cutter", shell_parameter_format: :cobra)
       end
     end
   end
