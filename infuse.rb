3a9af89690151bd20ce5c7976b47ad4b12732b48
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-04-10 18:28:05 -0600

    :rocket: Release infuse 0.1.2

--- infuse.rb
+++ infuse.rb
@@ -3,12 +3,12 @@ class Infuse < Formula
   homepage "https://github.com/oschrenk/infuse"
   license "MIT"
   head "https://github.com/oschrenk/infuse", branch: "main"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/oschrenk/infuse/releases/download/v#{version}/infuse-darwin-arm64"
-      sha256 "829d4d9fa00a892e3c1cb0a2dec8ad88d67c2b3f33f9a2c33b5467bfecafe857"
+      sha256 "9ba1c2a91e0900e5b7c4eaf3244cbf34e198a6a7e43715c6de1949dc11b178ee"
 
       def install
         chmod "+x", "infuse-darwin-arm64"
